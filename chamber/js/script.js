6fd67bc440278f7f8f186f14f99ed5103ae50217
moltomartin <moltomartin@gmail.com>
2022-05-22 00:54:45 -0300

    l05f

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,7 +1,21 @@
-function toggleMenu () {
+const now = new Date();
+const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+const currentYear = now.getFullYear();
+
+document.querySelector("currentdate").innerHTML = fullDate;
+document.querySelector("#copyrightupdatesmall_year").innerHTML = currentYear;
+document.getElementById("copyrightupdatesmall_lastup").innerHTML = document.lastModified;
+document.querySelector("#copyrightupdatemedium_year").innerHTML = currentYear;
+document.getElementById("copyrightupdatemedium_lastup").innerHTML = document.lastModified;
+document.querySelector("#copyrightupdatelarge_year").innerHTML = currentYear;
+document.getElementById("copyrightupdatelarge_lastup").innerHTML = document.lastModified;
+
+
+function toggleMenu() {
     document.getElementById("navigation").classList.toggle("open");
     document.getElementById("hambutton").classList.toggle("open");
 }
 
-const x = document.getElementById("hambutton")
+const x = document.getElementById("hambutton");
+
 x.onclick = toggleMenu;
