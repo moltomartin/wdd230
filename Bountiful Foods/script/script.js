eb2bd6f2259547bfede9cd4f3cb2b06964f01f4a
moltomartin <moltomartin@gmail.com>
2022-12-15 02:28:33 -0300

    boun

--- Bountiful Foods/script/script.js
+++ Bountiful Foods/script/script.js
@@ -11,12 +11,17 @@ x.onclick = toggleMenu;
 
 
 
-const options = {weekday: "long" , day: "numeric" , month: "long" , year: "numeric"};
-document.getElementById("currentDate01").textContent = new Date().toLocaleDateString("en-US" , options);
-
-
-const now = new Date();
-const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
-const currentYear = now.getFullYear();
-
-document.querySelector("#currentDate").innerHTML = fullDate;
+const date1 = document.querySelector("#actualdate");
+try {
+	const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		year: "numeric"
+	};
+	date1.innerHTML = `Last Update: <span class="highlight">${new Date().toLocaleDateString("en-UK", options)}</span>`;
+} catch (e) {
+	alert("Error with code or your browser does not support Locale");
+}
+let actualyear = document.querySelector("#actualyear");
+actualyear.innerHTML = `&copy 2022 | Martin Moltó | Mendoza Argentina`;
